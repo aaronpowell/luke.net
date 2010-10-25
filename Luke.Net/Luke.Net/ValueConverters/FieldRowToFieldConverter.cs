92a956ca8db99f20a80af1f88f5ef72b92dae63b
Mehdi Khalili <1029804+MehdiK@users.noreply.github.com>
2010-10-26 09:58:53 +1000

    Added filtering capabilities for terms view. For some reason the command parameter does not get evaluated; so selected terms do not contribute in filtering. WIP

---
+++ Luke.Net/Luke.Net/ValueConverters/FieldRowToFieldConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Luke.Net.Features.LuceneIndex;
+
+namespace Luke.Net.ValueConverters
+{
+    class FieldGridSelectedRowToFieldsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var grid = (DataGrid) value;
+            var selectedFields = grid.SelectedItems.Cast<FieldInfo>();
+            return selectedFields;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
