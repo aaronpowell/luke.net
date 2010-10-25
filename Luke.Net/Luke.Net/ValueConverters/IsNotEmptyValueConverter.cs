72ec779c4d29a90b837ac92cd3547d6c0b5ca136
Mehdi Khalili <1029804+MehdiK@users.noreply.github.com>
2010-10-25 13:38:41 +1000

    - Added a few converters - Changed menu enable/disable logic to work off binding in the xaml

---
+++ Luke.Net/Luke.Net/ValueConverters/IsNotEmptyValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Linq;
+
+namespace Luke.Net.ValueConverters
+{
+    class IsNotEmptyValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var enumerable = value as IEnumerable<object>;
+            if (enumerable == null)
+                return false;
+
+            return enumerable.Any();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
