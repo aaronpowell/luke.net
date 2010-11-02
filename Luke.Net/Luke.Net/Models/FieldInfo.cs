2eb77d69a6ed34f822af515626b562a39baa7810
Mehdi Khalili <armkhalili@gmail.com>
2010-11-02 16:49:58 +1000

    Changed the object model a bit: not TermInfo is an entity inside FieldInfo aggregate. Had to change a few models to make this work.

--- Luke.Net/Luke.Net/Models/FieldInfo.cs
+++ Luke.Net/Luke.Net/Models/FieldInfo.cs
@@ -1,9 +1,30 @@
-﻿namespace Luke.Net.Models
+﻿using System.Collections.Generic;
+
+namespace Luke.Net.Models
 {
     public class FieldInfo
     {
+        public FieldInfo()
+        {
+            _terms = new List<TermInfo>();
+        }
+
         public string Field { get; set; }
         public int Count { get; set; }
         public double Frequency { get; set; }
+        private readonly List<TermInfo> _terms;
+
+        public IEnumerable<TermInfo> Terms
+        {
+            get
+            {
+                return _terms;
+            }
+        }
+
+        public void AddTerm(TermInfo term)
+        {
+            _terms.Add(term);
+        }
     }
 }
--- Luke.Net/Luke.Net/Models/TermInfo.cs
+++ Luke.Net/Luke.Net/Models/TermInfo.cs
@@ -3,7 +3,7 @@
     public class TermInfo
     {
         public int Rank { get; set; }
-        public string Field { get; set; }
+        public FieldInfo Field { get; set; }
         public string Term { get; set; }
         public int Frequency { get; set; }
     }
