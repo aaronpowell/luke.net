646c031a34c7f0a5713b994b87d9c879ce5e5cae
Mehdi Khalili <armkhalili@gmail.com>
2010-11-03 07:16:03 +1000

    Rolled back some of the changes from the previous commit.

--- Luke.Net/Luke.Net/Models/LuceneIndex.cs
+++ Luke.Net/Luke.Net/Models/LuceneIndex.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Lucene.Net.Documents;
 using Luke.Net.Features.OpenIndex;
 using Lucene.Net.Index;
 using Luke.Net.Infrastructure.Utilities;
@@ -10,11 +9,6 @@ namespace Luke.Net.Models
 {
     public class LuceneIndex
     {
-        public LuceneIndex()
-        {
-            _documents = new List<DocumentInfo>();
-        }
-
         public OpenIndexModel OpenIndex { get; private set; }
 
         public FormatDetails IndexDetails { get; private set; }
@@ -29,14 +23,7 @@ namespace Luke.Net.Models
 
         public int DocumentCount { get; private set; }
 
-        private readonly List<DocumentInfo> _documents;
-        public IEnumerable<DocumentInfo> Documents
-        {
-            get
-            {
-                return _documents;
-            }
-        }
+        public IEnumerable<FieldInfo> Fields { get; private set; }
 
         public bool HasDeletions { get; private set; }
 
@@ -46,71 +33,55 @@ namespace Luke.Net.Models
 
         public void LoadIndex(OpenIndexModel indexInfo)
         {
+            //DirectoryInfo dir = new DirectoryInfo(indexInfo.IndexPath);
+            var directory = indexInfo.Directory;
             IndexReader indexReader = null;
 
             try
             {
-                var directory = indexInfo.Directory;
                 indexReader = IndexReader.Open(directory, indexInfo.ReadOnly);
-                var fieldCounter = new List<FieldInfo>();
-
-                for (var i = 0; i < indexReader.NumDocs(); i++)
-                {
-                    var doc = indexReader.Document(i);
-                    var fields = new List<FieldInfo>();
-                    foreach (var field in doc.GetFields().Cast<Field>())
-                    {
-                        var fieldInfo = new FieldInfo {Field = field.Name(), Value = doc.Get(field.Name())};
-                        fields.Add(fieldInfo);
-                        var terms = indexReader.Terms(new Term(field.Name(), string.Empty));
-                        while (terms.Next())
-                        {
-                            var term = terms.Term();
-                            fieldInfo.AddTerm(new TermInfo {Field = fieldInfo, Term = term.Text()});
-                        }
-                    }
-
-                    _documents.Add(new DocumentInfo(fields));
-                }
+                var v = IndexGate.GetIndexFormat(directory);
 
                 var termCount = 0;
-                //var terms = indexReader.Terms();
+                var terms = indexReader.Terms();
 
-                //var termCounter = new List<TermInfo>();
+                var fieldCounter = new List<FieldInfo>();
 
-                //while (terms.Next())
-                //{
-                //    var term = terms.Term();
-                //    var field = fieldCounter.SingleOrDefault(x => x.Field == term.Field());
+                var termCounter = new List<TermInfo>();
 
-                //    if(field != null)
-                //    {
-                //        field.Count++;
-                //    }
-                //    else
-                //    {
-                //        field = new FieldInfo {Count = 1, Field = term.Field()};
-                //        fieldCounter.Add(field);
-                //    }
+                while (terms.Next())
+                {
+                    var term = terms.Term();
+                    var field = fieldCounter.SingleOrDefault(x => x.Field == term.Field());
 
-                //    var termInfo = new TermInfo {Term = term.Text(), Field = field, Frequency = terms.DocFreq()};
-                //    termCounter.Add(termInfo);
-                //    field.AddTerm(termInfo);
+                    if(field != null)
+                    {
+                        field.Count++;
+                    }
+                    else
+                    {
+                        field = new FieldInfo {Count = 1, Field = term.Field()};
+                        fieldCounter.Add(field);
+                    }
 
-                //    termCount++;
-                //}
+                    var termInfo = new TermInfo {Term = term.Text(), Field = field, Frequency = terms.DocFreq()};
+                    termCounter.Add(termInfo);
+                    field.AddTerm(termInfo);
 
-                IndexDetails = IndexGate.GetFormatDetails(IndexGate.GetIndexFormat(directory));
+                    termCount++;
+                }
+
+                IndexDetails = IndexGate.GetFormatDetails(v);
                 FieldCount = indexReader.GetFieldNames(IndexReader.FieldOption.ALL).Count;
                 TermCount = termCount;
                 //LastModified = dir.LastWriteTime, // IndexReader.LastModified(directory);
                 Version = indexReader.GetVersion().ToString("x");
                 DocumentCount = indexReader.NumDocs();
-                //Fields = fieldCounter.Select(x =>
-                //                                 {
-                //                                     x.Frequency = ((double) x.Count*100)/termCount;
-                //                                     return x;
-                //                                 });
+                Fields = fieldCounter.Select(x =>
+                                                 {
+                                                     x.Frequency = ((double) x.Count*100)/termCount;
+                                                     return x;
+                                                 });
                 HasDeletions = indexReader.HasDeletions();
                 DeletionCount = indexReader.NumDeletedDocs();
                 Optimized = indexReader.IsOptimized();
