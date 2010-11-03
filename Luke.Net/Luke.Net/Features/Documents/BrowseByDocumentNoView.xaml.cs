4ae88574aefbb640aca1f4ca39876141bbe1636b
Mehdi Khalili <armkhalili@gmail.com>
2010-11-03 12:21:08 +1000

    Created all required documents views. No binding/view model/logic. Just the UI.

---
+++ Luke.Net/Luke.Net/Features/Documents/BrowseByDocumentNoView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Luke.Net.Features.Documents
+{
+    /// <summary>
+    /// Interaction logic for BrowseByDocumentNoView.xaml
+    /// </summary>
+    public partial class BrowseByDocumentNoView : UserControl
+    {
+        public BrowseByDocumentNoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
