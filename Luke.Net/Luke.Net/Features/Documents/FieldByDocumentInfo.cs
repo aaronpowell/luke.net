c2bb82e785b7db93ad66b4ed7af457a6ca1bb919
Mehdi Khalili <1029804+MehdiK@users.noreply.github.com>
2010-11-03 17:04:45 +1000

    Added missing flags to FieldByDocumentInfo and DocumentInfoView

--- Luke.Net/Luke.Net/Features/Documents/FieldByDocumentInfo.cs
+++ Luke.Net/Luke.Net/Features/Documents/FieldByDocumentInfo.cs
@@ -4,5 +4,14 @@
     {
         public string Field { get; set; }
         public string Value { get; set; }
+        public bool PositionTermVector { get; set; }
+        public bool OffsetTermVector { get; set; }
+        public bool Binary { get; set; }
+        public bool Lazy { get; set; }
+        public bool OmitTermFrequency { get; set; }
+        public bool OmitNorms { get; set; }
+        public bool Stored { get; set; }
+        public bool Tokenized { get; set; }
+        public bool Indexed { get; set; }
     }
 }
