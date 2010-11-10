0c9e44cb1088adc2eace18652402eaf7380410d4
Mehdi Khalili <armkhalili@gmail.com>
2010-11-10 14:56:43 +1000

    - Added parallelism support to FieldsView (and its model). - Added support to calculating fields properties after terms are loaded

--- Luke.Net/Luke.Net/Features/Overview/FieldsViewModel.cs
+++ Luke.Net/Luke.Net/Features/Overview/FieldsViewModel.cs
@@ -1,36 +1,66 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Input;
 using Luke.Net.Features.Overview.Services;
 using Luke.Net.Infrastructure;
-using Luke.Net.Models;
 using Microsoft.Practices.Prism.Events;
 using Microsoft.Practices.Prism.ViewModel;
+using System.Linq;
 
 namespace Luke.Net.Features.Overview
 {
     public class FieldsViewModel : NotificationObject
     {
-        private readonly IEventAggregator _eventAggregator;
         private readonly IIndexOverviewService _indexOverviewService;
         private readonly List<FieldInfo> _fields = new List<FieldInfo>();
 
         public FieldsViewModel(IEventAggregator eventAggregator, IIndexOverviewService indexOverviewService)
         {
-            _eventAggregator = eventAggregator;
             _indexOverviewService = indexOverviewService;
+            eventAggregator.GetEvent<TermsLoadedEvent>().Subscribe(TermsLoaded);
             InspectFields = new RelayCommand<IEnumerable<FieldInfo>>(InspectFieldsExecuted);
             Fields = new ListCollectionView(_fields);
 
             LoadModel();
         }
 
+        private void TermsLoaded(IEnumerable<TermInfo> terms)
+        {
+            // calculating fields properties after terms are loaded
+            ProgressText = "Calculating term count and percentage. Please wait ...";
+            var termsCount = terms.Count();
+
+            foreach (var field in _fields)
+            {
+                FieldInfo field1 = field;
+                field.Count = terms.Count(t => t.Field == field1.Field);
+                field.Frequency = ((double)field.Count*100)/termsCount;
+            }
+
+            Fields.Refresh();
+            IsLoading = false;
+        }
+
         private void LoadModel()
         {
             _fields.Clear();
-            _fields.AddRange(_indexOverviewService.GetFields());
-            Fields.Refresh();
+
+            IsLoading = true;
+            ProgressText = "Loading fields from index. Please wait ...";
+            var ui = TaskScheduler.FromCurrentSynchronizationContext();
+
+            Task.Factory
+                .StartNew(() => _indexOverviewService.GetFields().ToList())
+                .ContinueWith(
+                    t =>
+                        {
+                            _fields.AddRange(t.Result);
+                            Fields.Refresh();
+                            ProgressText = "Waiting for terms to load. Please wait ...";
+                        },
+                    ui);
         }
 
         public ICommand InspectFields { get; set; }
@@ -40,5 +70,27 @@ namespace Luke.Net.Features.Overview
         }
 
         public ICollectionView Fields { get; private set; }
+
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            private set
+            {
+                _isLoading = value;
+                RaisePropertyChanged(() => IsLoading);
+            }
+        }
+
+        private string _progressText;
+        public string ProgressText
+        {
+            get { return _progressText; }
+            private set
+            {
+                _progressText = value;
+                RaisePropertyChanged(()=>ProgressText);
+            }
+        }
     }
 }
